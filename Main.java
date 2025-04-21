4518f96d79974a14662fc89ac2daccd33b041104
soham pankaj mamdyal <75490885+sohammamdyal@users.noreply.github.com>
2025-04-21 22:37:20 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("This is java program 2");
+        System.out.println(new Date());
+        System.out.println("This is java program 3");
+        System.out.println("This is java program 4");
+    }
+}
