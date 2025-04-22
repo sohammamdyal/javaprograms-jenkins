648493ddd297c56c105c0c37081cf9d04d06ef3f
soham pankaj mamdyal <75490885+sohammamdyal@users.noreply.github.com>
2025-04-22 12:06:19 +0530

    added one more row

--- Main.java
+++ Main.java
@@ -10,5 +10,7 @@ public class Main {
         System.out.println("This is java program 5");
 
         System.out.println("This is java program 6");  
+
+        System.err.println("This is java program 7");
     }
 }
