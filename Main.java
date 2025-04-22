99894fdfb3633ee55aa21d75e21af53812632de0
soham pankaj mamdyal <75490885+sohammamdyal@users.noreply.github.com>
2025-04-22 12:08:05 +0530

    added more line edit

--- Main.java
+++ Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println("This is java program 6");  
 
         System.err.println("This is java program 7");
+        System.err.println("This is java program 8");   
     }
 }
